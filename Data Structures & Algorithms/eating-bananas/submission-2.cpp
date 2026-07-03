04ca14b524d06aea9fbe8c292b03f15e388c3de7
Mirnes Fehrić <mfehric2@etf.unsa.ba>
2026-07-03 12:50:25 +0200

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maks = 1;
+        int n = piles.size();
+        for(auto& x: piles){
+            maks = max(maks,x);
+        }
+        int poc = 1;
+        int kraj = maks;
+        int bestkorak = 1000000000;
+        int ans = 0;
+        while(poc <= kraj){
+            int korak = 0;
+            int mid = poc + (kraj-poc)/2;
+            cout << "MID JE: " << mid << endl;
+            for(const auto& x: piles){
+                korak+= (x+mid-1)/mid;
+            }
+            if(korak <= h){
+                kraj = mid-1;
+                ans = mid;
+            }else{
+                poc = mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
