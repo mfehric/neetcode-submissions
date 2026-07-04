d601fe8f0209944ecd85cbf1e5ac0238b08f846a
Mirnes Fehrić <mfehric2@etf.unsa.ba>
2026-07-04 15:57:47 +0200

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> output;
+        for(int i = 0; i <= n; i++){
+            output.push_back(__builtin_popcount(i));
+        }
+        return output;
+    }
+};
