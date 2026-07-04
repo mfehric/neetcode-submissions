4f2c378566124bfb94db955aa2bdcae6bf813f22
Mirnes Fehrić <mfehric2@etf.unsa.ba>
2026-07-04 16:26:08 +0200

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t m = 0;
+        for(int i = 0; i < 32; i++){
+            if(n&(1<<i)){
+                m += 1<<(31-i);
+            }
+            
+        }
+        cout << bitset<32>(m);
+        return m;
+        
+    }
+};
