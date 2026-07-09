7f91491662c148b64d06bcb97e084f8df86cfca0
Mirnes Fehrić <mfehric2@etf.unsa.ba>
2026-07-09 08:06:52 +0200

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.cpp
@@ -0,0 +1,61 @@
+/*class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        bool preljevanje = true;
+        for(int i = 0; i < n; i++){
+            if(digits[i] != 9){
+                preljevanje = false;
+                break;
+            }
+        }
+        if(preljevanje){
+            for(int i = 0;  i < n; i++){
+                if(i != 0){
+                    digits[i] = 0;
+                }else{
+                    digits[i] = 1;
+                }
+            }
+            digits.push_back(0);
+            return digits;
+        }else{
+            int carry = 0;
+            if(digits[n-1]+1+carry >= 10){
+                digits[n-1] = (digits[n-1]+1+carry)%10;
+                carry = 1;
+            }else{
+                digits[n-1] = digits[n-1]+1+carry;
+            }
+            
+            
+            for(int i = n-2; i >= 0; i--){
+                if(digits[i]+carry >= 10){
+                    digits[i] = (digits[i]+carry)%10;
+                }else{
+                    digits[i] += carry;
+                    break;
+                }
+            }
+            return digits;
+        }
+    }
+};*/
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        vector<int> result(n + 1);
+        result[0] = 1;
+        return result;
+    }
+};
