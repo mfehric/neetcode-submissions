b4095e628f0d0e5c42df5e4086df42b25ea7c512
Mirnes Fehrić <mfehric2@etf.unsa.ba>
2026-07-09 09:31:22 +0200

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int top_row = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0;  i< n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0){
+                        top_row = 0;
+                    }else{
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                cout << matrix[i][j] << " "; 
+            }
+            cout << "\n";
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(top_row == 0){
+            for(int j = 1; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int i = 1; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(top_row == 0){
+            matrix[0][0] = 0;
+        }
+    }
+};
